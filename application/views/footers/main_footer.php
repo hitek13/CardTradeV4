e433b6c7c16fdace394a2fc32cdbe688d3116b99
hitek13 <13planetas.enanos@gmail.com>
2017-01-22 14:51:21 +0100

    Añadido header y footer

--- application/views/footers/main_footer.php
+++ application/views/footers/main_footer.php
@@ -1,4 +1,90 @@
 
+    <footer class="container-fluid">
+        <div class="row ">
+          <div class="visible-xs-block">
+              <div role="tabpanel">
+
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs nav-stacked text-center" role="tablist">
+          <li role="presentation" class="active"><a href="#harzspk" aria-controls="harzspk" role="tab" data-toggle="tab">Harzsparkasse</a></li>
+          <li role="presentation"><a href="#volksbank" aria-controls="volksbank" role="tab" data-toggle="tab">Harzer Volksbank</a></li>
+          <li role="presentation"><a href="#links" aria-controls="links" role="tab" data-toggle="tab">Weitere Links</a></li>
+
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane fade in active" id="harzspk">
+                  <h3>Harzsparkasse</h3>
+                        <ul class="list-group ">
+                                              <li class="list-group-item">Harzsparkasse</li>
+                                              <li class="list-group-item">BIC: NOLADE21HRZ</li>
+                                              <li class="list-group-item">IBAN: DE 16 810 520 000 300 64 59 61</li>
+
+                                      </ul>
+          </div>
+          <div role="tabpanel" class="tab-pane fade" id="volksbank">
+              <h3>Harzer Volksbank</h3>
+                              <ul class="list-group">
+                                              <li class="list-group-item">Harzer Volksbank</li>
+                                              <li class="list-group-item">BIC: 
+                              GENODEF1QLB</li>
+                                              <li class="list-group-item">IBAN: 
+                              DE 65 8006 3508 5100 2701 00</li>
+                                              <li class="list-group-item">link4</li>
+                                      </ul>
+          </div>
+          <div role="tabpanel" class="tab-pane fade" id="links">    
+                  <h3>Weiter Informationen finden Sie hier</h3>
+                              <ul class="list-group">
+                                      <li class="list-group-item"><a href="#">Facebook</a></li>
+                                      <li class="list-group-item"><a href="#">Myspace</a></li>
+                                      <li class="list-group-item">link3</li>
+                                      <li class="list-group-item">link4</li>
+                              </ul></div>
+
+        </div>
+
+      </div>
+          </div>
+              <div class="hidden-xs">
+              <div class="col-md-4 col-lg-4 col-sm-4 ">
+
+                                      <h3>Harzsparkasse</h3>
+                        <ul class="list-group ">
+                                              <li class="list-group-item">Harzsparkasse</li>
+                                              <li class="list-group-item">BIC: NOLADE21HRZ</li>
+                                              <li class="list-group-item">IBAN: DE 16 810 520 000 300 64 59 61</li>
+
+                                      </ul>
+                              </div>
+                              <div class="col-md-4 col-lg-4 col-sm-4 ">
+                    <h3>Harzer Volksbank</h3>
+                                      <ul class="list-group">
+                                              <li class="list-group-item">Harzer Volksbank</li>
+                                              <li class="list-group-item">BIC: 
+                              GENODEF1QLB</li>
+                                              <li class="list-group-item">IBAN: 
+                              DE 65 8006 3508 5100 2701 00</li>
+                                              <li class="list-group-item">link4</li>
+                                      </ul>
+                              </div>
+
+                      <div class="col-md-4 col-lg-4 col-sm-4">
+                      <h3>Weiter Informationen finden Sie hier</h3>
+                              <ul class="list-group">
+                                      <li class="list-group-item"><a href="#">Facebook</a></li>
+                                      <li class="list-group-item"><a href="#">Myspace</a></li>
+                                      <li class="list-group-item">link3</li>
+                                      <li class="list-group-item">link4</li>
+                              </ul>
+                      </div>
+                      </div>
+
+              </div>
+      </footer>
+
+
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
--- application/views/headers/main_header.php
+++ application/views/headers/main_header.php
@@ -12,16 +12,78 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <link rel="icon" href="../../favicon.ico">
         
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+            integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
         <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+        </script>
 
 	<title>Welcome to CodeIgniter</title>
+        
+        <style>
+            footer .list-group-item{
+                border:0;
+                background:transparent;
+                }
+
+                div.visible-xs-block
+                {
+                        background:maroon;
+                        color:white;
+                        font-family:"Tahoma" serif;
+                        padding:1em;
+                    }
+
+                    footer{
+                        background:#555;
+                        color:white;
+                       height:auto;
+                        }
+        </style>
 
 	
 </head>
 <body>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">FoW Card-Trade</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+    </nav>
+
+        <dib class="container">
+            <div class="row-fluid">
+                <div clas="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <h1>FoW Card-Trade</h1>
+                </div>
+                <div clas="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <form>
+                        <div class="input-group col-xs-4">
+                            <input type="text" class="form-control input-lg" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default input-lg" type="submit">
+                                  <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </dib>
+ 
